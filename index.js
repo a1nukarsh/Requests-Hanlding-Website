8ca4fd5c35f4fcebdda0b3169ca329000cc91d1f
Anukarsh <anukarshr@gmail.com>
2021-10-30 22:41:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 console.log("Hello this is post website");
 
+// Utility functions:
+// 1. Utility Function to get DOM element
+function getElementFromStr(string) {
+  let div = document.createElement("div");
+  div.innerHTML = string;
+  return string.firstElementChild;
+}
+
 // Initialize parameter number
-let addedParamsCount = 0
+let addedParamsCount = 0;
 
 // Hide the parameters box
 let parametersBox = document.getElementById("parametersBox");
@@ -27,18 +35,30 @@ addParams.addEventListener("click", () => {
   let params = document.getElementById("params");
   let string = `
     <div class="form row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Parameter ${addedParamsCount + 2}</label>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Parameter ${
+                  addedParamsCount + 2
+                }</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control" id="parameterKey${addedParamsCount + 2}" placeholder="Enter Parameter ${addedParamsCount + 2} Key">
+                    <input type="text" class="form-control" id="parameterKey${
+                      addedParamsCount + 2
+                    }" placeholder="Enter Parameter ${
+    addedParamsCount + 2
+  } Key">
                     <!-- <input type="text" class="form-control" id="urlField"> -->
                 </div>
                 <div class="col-md-4" style="display: flex;">
-                    <input type="text" class="form-control" id="parameterValue${addedParamsCount + 2}" placeholder="Enter Parameter ${addedParamsCount + 2} Value">
+                    <input type="text" class="form-control" id="parameterValue${
+                      addedParamsCount + 2
+                    }" placeholder="Enter Parameter ${
+    addedParamsCount + 2
+  } Value">
                     <!-- <input type="text" class="form-control" id="urlField"> -->
                     <button class="btn btn-primary" id="deleteParams" style="margin-left: 8px;">-</button>
                 </div>
             </div>
     `;
-    addedParamsCount ++
+    // Convert the element to DOM node
+    let paramElement = getElementFromStr(string)
+    console.log(paramElement)
+  addedParamsCount++;
 });
-
