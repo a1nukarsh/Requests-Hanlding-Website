193fdda8aff01850dad7f1e621ab34512ca95f02
Anukarsh <anukarshr@gmail.com>
2021-11-08 22:34:46 +0530

    update

--- index.js
+++ index.js
@@ -29,37 +29,55 @@ jsonRadio.addEventListener("click", () => {
   document.getElementById("requestJsonBox").style.display = "block";
 });
 
-// if user clicks on + button add more parameters
+// if user clicks on + button add more parameters or - button to delete parameters
 let addParams = document.getElementById("addParams");
 addParams.addEventListener("click", () => {
   let params = document.getElementById("params");
   let string = `
     <div class="form row my-3">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Parameter ${
-                  addedParamsCount + 2
-                }</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" id="parameterKey${
-                      addedParamsCount + 2
-                    }" placeholder="Enter Parameter ${
-    addedParamsCount + 2
-  } Key">
-                    <!-- <input type="text" class="form-control" id="urlField"> -->
-                </div>
-                <div class="col-md-4" style="display: flex;">
-                    <input type="text" class="form-control" id="parameterValue${
-                      addedParamsCount + 2
-                    }" placeholder="Enter Parameter ${
-    addedParamsCount + 2
-  } Value">
-                    <!-- <input type="text" class="form-control" id="urlField"> -->
-                    <button class="btn btn-primary" id="deleteParams" style="margin-left: 8px;">-</button>
-                </div>
-            </div>
+      <label for="inputPassword" class="col-sm-2 col-form-label">
+        Parameter ${addedParamsCount + 2}
+      </label>
+      <div class="col-md-4">
+          <input type="text" class="form-control" id="parameterKey${addedParamsCount + 2}" placeholder="Enter Parameter ${addedParamsCount + 2} Key">
+          <!-- <input type="text" class="form-control" id="urlField"> -->
+      </div>
+      <div class="col-md-4" style="display: flex;">
+          <input type="text" class="form-control" id="parameterValue${addedParamsCount + 2}" placeholder="Enter Parameter ${addedParamsCount + 2} Value">
+            <button class="btn btn-primary remove deleteParams" style="margin-left: 8px;"> - </button>
+      </div>
+    </div>
     `;
   // Convert the element to DOM node
   let paramElement = getElementFromStr(string);
   // console.log(paramElement);
   params.appendChild(paramElement);
+
+  // Add an event listener to remove the parameter on clicking the - button
+  let deleteParams = document.getElementsByClassName("deleteParams");
+  for (item of deleteParams) {
+      item.addEventListener("click", (e) => {
+        e.target.parentElement.parentElement.remove();
+      });
+  }
   addedParamsCount++;
 });
+
+// If the user clicks on Submit button
+let submit = document.getElementById('submit')
+submit.addEventListener('click', ()=>{
+  // Show please wait in the response box
+  document.getElementById('responseText').value = "Please Wait, Fetching Response..."
+
+  // Fetch all the values user has entered
+  let url = document.getElementById('urlField').value
+  let requestType = document.querySelector("input[name='requestType']:checked").value
+  let contentType = document.querySelector("input[name='contentType']:checked").value
+
+  // Logging the values in console
+  console.log('Url = ', url)
+  console.log('rType = ', requestType)
+  console.log('ctype = ', contentType)
+
+  // if user selects 
+})
